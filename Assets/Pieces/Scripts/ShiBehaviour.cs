a9463ef14cacab6aa440be7696bdb07376144f23
Robin <robingiraudon@hotmail.fr>
2018-03-25 22:28:44 +0100

    Add the advisor (Shi) piece

---
+++ Assets/Pieces/Scripts/ShiBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class ShiBehaviour : PieceBehaviour {
+
+    public override bool movement_Possible(int xNewPosition, int yNewPosition)
+    {
+        bool diagonalMov = (Math.Abs(yPosition - yNewPosition) == 1) && (Math.Abs(xPosition - xNewPosition) == 1);
+
+
+        //check if still in palace 
+        bool inPalace;
+        if (redPiece)
+        {
+            inPalace = (yNewPosition <= 2) && (xNewPosition >= 3) && (xNewPosition <= 5);
+        }
+        else
+        {
+            inPalace = (yNewPosition >= 7) && (xNewPosition >= 3) && (xNewPosition <= 5);
+        }
+
+        return inPalace && diagonalMov;
+    }
+}
